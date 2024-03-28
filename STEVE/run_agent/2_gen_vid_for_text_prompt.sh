e1294fdef3466fe59fd9d8ec2caf7ab43e323210
Alex-Tang-0113 <yytang0113@gmail.com>
2024-03-28 20:54:46 +0000

    bash

--- STEVE/run_agent/2_gen_vid_for_text_prompt.sh
+++ STEVE/run_agent/2_gen_vid_for_text_prompt.sh
@@ -1,11 +1,20 @@
+
 # Change custom_text_prompt to whatever text prompt you want to generate a video
+for i in {1..5}; do \
 xvfb-run python steve1/run_agent/run_agent.py \
 --in_model data/weights/vpt/2x.model \
 --in_weights data/weights/steve1/steve1.weights \
 --prior_weights data/weights/steve1/steve1_prior.pt \
 --text_cond_scale 6.0 \
 --visual_cond_scale 7.0 \
---gameplay_length 6000 \
+--gameplay_length 10000 \
 --save_dirpath data/generated_videos/custom_text_prompt \
---custom_text_prompt_pth /home/user/ltl_steve1/STEVE/steve1/data/prompts.txt \
---custom_conditions_pth /home/user/ltl_steve1/STEVE/steve1/data/conditions.txt
+--task collect_stone \
+--start_template_pth /home/user/ltl_steve1/STEVE/steve1/data/prompt/start_prompt.json \
+--next_template_pth /home/user/ltl_steve1/STEVE/steve1/data/prompt/next_prompt.json \
+--help_template_pth /home/user/ltl_steve1/STEVE/steve1/data/prompt/help_prompt.json \
+--stuckpoint_pth /home/user/ltl_steve1/STEVE/steve1/data/prompt \
+--help_gap 2000
+# --custom_text_prompt_pth /home/user/ltl_steve1/STEVE/steve1/data/prompts.txt \
+# --custom_conditions_pth /home/user/ltl_steve1/STEVE/steve1/data/conditions.txt; \
+done
